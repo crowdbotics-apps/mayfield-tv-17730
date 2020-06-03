22278ebfa547bee75ccab8c6c31f0d4948ca6ecb
Crowdbotics <team@crowdbotics.com>
2020-06-03 02:51:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mayfield_tv_17730/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mayfield_tv_17730;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "mayfield_tv_17730";
+    }
+}
